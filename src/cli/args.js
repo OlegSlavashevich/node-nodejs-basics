e64efeed92e4f431646d0c63b09b63b7939d80ce
Slavashevich <aslavashevich@ibagroup.eu>
2023-06-14 11:15:55 +0300

    Command line interface(src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    const argsAsArray = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+        argsAsArray.push([args[i].slice(2), args[i + 1]]);
+    }
+
+    const result = argsAsArray.map((ar => `${ar[0]} is ${ar[1]}`)).join(', ');
+
+    console.log(result);
 };
 
 parseArgs();
