b9a6f5da7b20dffee26c5502b9587405e05cca88
Slavashevich <aslavashevich@ibagroup.eu>
2023-06-16 13:31:43 +0300

    Streams (src/streams)

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from 'fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = `${__dirname}/files/fileToRead.txt`;
+
+    const readableStream = fs.createReadStream(filePath);
+
+    readableStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
