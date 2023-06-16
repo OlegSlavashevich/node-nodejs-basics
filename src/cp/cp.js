e512bd7b0321007fee6101d2f5603aac68ddf0ea
Slavashevich <aslavashevich@ibagroup.eu>
2023-06-16 16:05:41 +0300

    Child Processes (src/cp)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { spawn } from 'node:child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', [`${__dirname}/files/script.js`, ...args]);
+
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['firstArg', 'secArg']);
